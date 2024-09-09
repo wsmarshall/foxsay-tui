24d27fb053a6ce78b6613f9e612bfb73375de07f
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-09-09 12:56:13 -0700

    adds esc key to quit fnality

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use cursive::views::TextView; //Views form main bldg blocks of a Cursive TUI program
                               //cursive::views contains 'pre built views' like buttons, checkboxes, etc.
+use cursive::event::Key;
 
 fn main() {
     let mut siv = cursive::default(); //creates a Cursive root object
@@ -13,6 +14,10 @@ fn main() {
 
     //declare app layout - holds fox ASCII art, has to be added as layer to be visible in main program
     siv.add_layer(TextView::new(fox_text));
+
+    // Listen for escape key press, which exist program
+    //two args, events and callback fn
+    siv.add_global_callback(Key::Esc, |k| k.quit());
 
     siv.run(); //starts main event loop
 }
