e82fe77fb573eaba836b82c6a3735d09298a031e
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-09-10 11:49:12 -0700

    shortens TextView wrap with Dialog::text

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,8 @@ fn main() {
       =(  I  )=";
 
     //declare app layout - holds fox ASCII art, has to be added as layer to be visible in main program
-    siv.add_layer(Dialog::around(TextView::new(fox_text)).button("OK", |k| k.quit())); // Listen for escape key press, which exist program
-                                                                                       //two args, events and callback fn
+    siv.add_layer(Dialog::text(fox_text).button("OK", |k| k.quit())); // Listen for escape key press, which exist program
+                                                                      //two args, events and callback fn
 
     siv.run(); //starts main event loop
 }
