69b7412354438cbd886e952871c2494b718f3319
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-09-10 11:48:11 -0700

    adds Dialogue (removes Key (explicit event listener))

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use cursive::views::TextView; //Views form main bldg blocks of a Cursive TUI program
-                              //cursive::views contains 'pre built views' like buttons, checkboxes, etc.
-use cursive::event::Key;
+use cursive::views::{Dialog, TextView}; //Views form main bldg blocks of a Cursive TUI program
+                                        //cursive::views contains 'pre built views' like buttons, checkboxes, etc.
 
 fn main() {
     let mut siv = cursive::default(); //creates a Cursive root object
@@ -13,11 +12,8 @@ fn main() {
       =(  I  )=";
 
     //declare app layout - holds fox ASCII art, has to be added as layer to be visible in main program
-    siv.add_layer(TextView::new(fox_text));
-
-    // Listen for escape key press, which exist program
-    //two args, events and callback fn
-    siv.add_global_callback(Key::Esc, |k| k.quit());
+    siv.add_layer(Dialog::around(TextView::new(fox_text)).button("OK", |k| k.quit())); // Listen for escape key press, which exist program
+                                                                                       //two args, events and callback fn
 
     siv.run(); //starts main event loop
 }
